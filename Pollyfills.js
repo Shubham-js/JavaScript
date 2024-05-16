b0751d11e3e09421781d933cf581cd205c492aa0
shubham-js <shubbhamcomputer33@gmail.com>
2024-05-17 01:34:24 +0530

    Pollyfill Addition

--- Pollyfills.js
+++ Pollyfills.js
@@ -348,8 +348,6 @@ arr.myForEach(testFunction);
 // );
 // console.log(message);
 
-
-
 const debounce = (cb, d) => {
   let timer;
   return function (...args) {
@@ -382,92 +380,129 @@ const throttledCall = throttle((data) => {
   console.log("inside click ", data);
 }, 3000);
 
-
 // Promise.all Promise.allSettle
 
-const p1 = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
     resolve("Promise 1 resolved");
   }, 2500);
-})
-const p2 = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
     resolve("Promise 2 resolved");
   }, 500);
-})
-const p3 = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
+});
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
     resolve("Promise 3 resolved");
   }, 3500);
-})
-const p4 = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
+});
+const p4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
     reject("Promise 4 resolved");
   }, 1500);
-})
+});
 
-// async function PromiseAll(promises){
-
-//   return new Promise((resolve,reject)=>{
-//     if(promises.length===0) {
-//       resolve(result);
-//     }
-//       let result=[];
-//       let count = 0;
-//     promises.forEach((promise,index) => {
-//             Promise.resolve(promise).then((data) => {
-//          result[index] = data;
-//          count++
-//          if(count === promises.length) {
-//              resolve(result);
-//          }
-//      }).catch((err) => {
-//          reject(err)
-//      })
-//     })
-    
- 
- 
-//   })
-  
-// };
+async function PromiseAll(promises) {
+  return new Promise((resolve, reject) => {
+    if (promises.length === 0) {
+      resolve(result);
+    }
+    let result = [];
+    let count = 0;
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((data) => {
+          result[index] = data;
+          count++;
+          if (count === promises.length) {
+            resolve(result);
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  });
+}
 
 // PromiseAll([p1,p2,p3,p4]).then(res=>console.log(res)).catch(err=>console.log(err));
 
-
-// async function PromiseAllSettled(promises){
-
-//   return new Promise((resolve,reject)=>{
-//     if(promises.length===0) {
-//       resolve(result);
-//     }
-//       let result=[];
-//       let count = 0;
-//     promises.forEach((promise,index) => {
-//             Promise.resolve(promise).then((data) => {
-//          result[index] = {
-//              isFulfilled:true,
-//              value:data
-             
-//          }
-//      }).catch((err) => {
-//         result[index] = {
-//              isFulfilled:false,
-//              value:err
-//         }
-        
-//      }).finally(()=>{
-//          count++
-//          if(count === promises.length) {
-//              resolve(result);
-//          }
-//      })
-//     })
-    
- 
- 
-//   })
-  
-// };
+async function PromiseAllSettled(promises) {
+  return new Promise((resolve, reject) => {
+    if (promises.length === 0) {
+      resolve(result);
+    }
+    let result = [];
+    let count = 0;
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((data) => {
+          result[index] = {
+            isFulfilled: true,
+            value: data,
+          };
+        })
+        .catch((err) => {
+          result[index] = {
+            isFulfilled: false,
+            value: err,
+          };
+        })
+        .finally(() => {
+          count++;
+          if (count === promises.length) {
+            resolve(result);
+          }
+        });
+    });
+  });
+}
 
 // PromiseAllSettled([p1,p2,p3,p4]).then(res=>console.log(res)).catch(err=>console.log(err));
+
+// First settled promise whether resolved or rejected.
+async function PromiseRace(promises) {
+  return new Promise((resolve, reject) => {
+    if (promises.length === 0) {
+      resolve(result);
+    }
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  });
+}
+
+// PromiseRace([p1,p2,p3,p4]).then(res=>console.log(res)).catch(err=>console.log(err));
+
+// First settled promise only when resolved, if all are rejected then gives the error.
+async function PromiseAny(promises) {
+  let errors = [];
+  let errCnt = promises.length;
+  return new Promise((resolve, reject) => {
+    if (promises.length === 0) {
+      resolve(result);
+    }
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          errors[index] = err;
+          errCnt--;
+          if (errCnt == 0) {
+            reject(AggregateError(errors));
+          }
+        });
+    });
+  });
+}
+
+// PromiseAny([p1,p2,p3,p4]).then(res=>console.log(res)).catch(err=>console.log(err));
